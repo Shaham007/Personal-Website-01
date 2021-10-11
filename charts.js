4468ea791c90cc4e8e7de02cf613393eefc3a16a
Shaham007 <80675121+Shaham007@users.noreply.github.com>
2021-10-12 01:23:36 +0200

    Add files via upload

---
+++ charts.js
@@ -0,0 +1,29 @@
+
+var ctx = document.getElementById('Chart').getContext('2d');
+var Chart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ['HTML', 'CSS', 'JavaScript', 'React.js'],
+        datasets: [{
+            label: '# of Votes',
+            data: [15, 15, 8, 5],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
+ 
